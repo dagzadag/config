8e1a55ed8298e7ac3e39705152e32e6135a7e372
dagzadag <37269752+dagzadag@users.noreply.github.com>
2019-04-13 12:18:36 +0200

    init

---
+++ firebaseconfig.js
@@ -0,0 +1,9 @@
+var config = {
+			apiKey: "AIzaSyACSgbAIM01KGI_twhGzX1FABWyhhCS3Eo",
+			authDomain: "test-6ee2a.firebaseapp.com",
+			databaseURL: "https://test-6ee2a.firebaseio.com",
+			projectId: "test-6ee2a",
+			storageBucket: "test-6ee2a.appspot.com",
+			messagingSenderId: "448582725279"
+	};
+	firebase.initializeApp(config);
