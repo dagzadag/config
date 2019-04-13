20abac054285002627fde22da910605eb187edf2
dagzadag <37269752+dagzadag@users.noreply.github.com>
2019-04-13 14:35:59 +0200

    updatefirebase

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -7,3 +7,21 @@ var config = {
 			messagingSenderId: "448582725279"
 	};
 	firebase.initializeApp(config);
+function submitToFirebase(){
+  var firebaseRef = firebase.database().ref()
+  var a = document.getElementsByClassName('visualCaptcha-selected')[0].innerHTML
+  var indesNum =  a.search('id=')
+  var idNum = a.slice(indesNum + 22 ,indesNum + 23 )
+  var expl = document.getElementsByClassName('visualCaptcha-explanation')[0].innerHTML
+  var sli = expl.slice(29)
+  var indeNum = sli.search("<")
+  var finalSli = sli.slice(0,indeNum)
+  
+  var element = document.getElementById("visualCaptcha-img-"+idNum);
+	html2canvas(element).then(function(canvas) {
+    // Export the canvas to its data URI representation
+    var base64image = canvas.toDataURL("image/png");
+    // Open the image in a new window
+    firebaseRef.child(finalSli).set(base64image);
+	});
+}
